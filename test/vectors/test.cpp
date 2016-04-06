efbd7150bd4633b2d132a6ae2a199cb05428ab1b
Lennart <lennart@mooxmirror.io>
2016-04-06 17:03:56 +0200

    test: fix message format

--- test/vectors/test.cpp
+++ test/vectors/test.cpp
@@ -16,31 +16,31 @@ int main(int argc, char const *argv[]) {
 	Vector smallVector(0);
 
 	// check equality operator
-	assertTrue(nullVector1 == nullVector2, "Vector{0, 0, 0} == Vector(3)");
-	assertError([&](){ nullVector1 == smallVector; }, "Vector{0, 0, 0} == Vector(0)");
+	assertTrue(nullVector1 == nullVector2, "{0, 0, 0} == Vector(3)");
+	assertError([&](){ nullVector1 == smallVector; }, "{0, 0, 0} == Vector(0)");
 
 	Vector identityVector1 = {1, 0, 0};
 	Vector identityVector2 = {0, 1, 0};
 	Vector identityVector3 = {0, 0, 1};
 
 	// check unequality operator
-	assertTrue(identityVector1 != identityVector2, "Vector{1, 0, 0} != Vector{0, 1, 0}");
-	assertError([&](){ identityVector1 != smallVector; }, "Vector{1, 0, 0} != Vector(0)");
+	assertTrue(identityVector1 != identityVector2, "{1, 0, 0} != {0, 1, 0}");
+	assertError([&](){ identityVector1 != smallVector; }, "{1, 0, 0} != Vector(0)");
 
 	Vector vectorSum1 = identityVector1 + identityVector2;
 	Vector vectorSum2 = vectorSum1 + identityVector3;
 	Vector vectorSum3 = {1, 1, 1};
 
 	// check add operator
-	assertTrue(vectorSum2 == vectorSum3, "Vector{1, 0, 0} + Vector{0, 1, 0} + Vector{0, 0, 1} == Vector{1, 1, 1}");
-	assertError([&]() { vectorSum1 + smallVector; }, "Vector{1, 0, 0} + Vector(0)");
+	assertTrue(vectorSum2 == vectorSum3, "{1, 0, 0} + {0, 1, 0} + {0, 0, 1} == {1, 1, 1}");
+	assertError([&]() { vectorSum1 + smallVector; }, "{1, 0, 0} + Vector(0)");
 
 	Vector vectorSub1 = vectorSum1 - identityVector1;
 	Vector vectorSub2 = vectorSub1 - identityVector2;
 
 	// check subtraction operator
-	assertTrue(vectorSub1 == identityVector2, "Vector{1, 1, 0} - Vector{1, 0, 0} == Vector{0, 1, 0}");
-	assertError([&](){ vectorSub1 - smallVector; }, "Vector{0, 1, 0} - Vector(0)");
+	assertTrue(vectorSub1 == identityVector2, "{1, 1, 0} - {1, 0, 0} == {0, 1, 0}");
+	assertError([&](){ vectorSub1 - smallVector; }, "{0, 1, 0} - Vector(0)");
 
 	Vector vectorScalar1 { 2, 2, 2 };
 	Vector vectorScalar2 { 0.5, 0.5, 0.5 };
@@ -50,23 +50,23 @@ int main(int argc, char const *argv[]) {
 	DOUBLE scalar3 = -1.0;
 
 	// check scalar multiplication
-	assertTrue(vectorSum3 * scalar1 == vectorScalar1, "Vector {1, 1, 1} * 2.0 == Vector {2.0, 2.0, 2.0}");
-	assertTrue(vectorSum3 / scalar1 == vectorScalar2, "Vector {1, 1, 1} / 2.0 == Vector {0.5, 0.5, 0.5}");
-	assertTrue(vectorSum3 * scalar2 == nullVector1, "Vector {1, 1, 1} * 0 == Vector {0, 0, 0}");
-	assertTrue(vectorSum3 * scalar3 == vectorScalar3, "Vector {1, 1, 1} * -1 == Vector {-1, -1, -1}");
+	assertTrue(vectorSum3 * scalar1 == vectorScalar1, "{1, 1, 1} * 2.0 == {2.0, 2.0, 2.0}");
+	assertTrue(vectorSum3 / scalar1 == vectorScalar2, "{1, 1, 1} / 2.0 == {0.5, 0.5, 0.5}");
+	assertTrue(vectorSum3 * scalar2 == nullVector1, "{1, 1, 1} * 0 == {0, 0, 0}");
+	assertTrue(vectorSum3 * scalar3 == vectorScalar3, "{1, 1, 1} * -1 == {-1, -1, -1}");
 
 	// check vector inversion
-	assertTrue(-vectorSum3 == vectorScalar3, "-Vector {1, 1, 1} == Vector {-1, -1, -1}");
+	assertTrue(-vectorSum3 == vectorScalar3, "-{1, 1, 1} == {-1, -1, -1}");
 
 	// check dot product operator
 	DOUBLE dotProduct1 = 1;
 	DOUBLE dotProduct2 = 0;
 	DOUBLE dotProduct3 = 3;
 
-	assertTrue(vectorSum3.dot(identityVector1) == dotProduct1, "Vector {1, 1, 1} (dot) Vector {1, 0, 0} == 1");
-	assertTrue(identityVector1.dot(identityVector2) == dotProduct2, "Vector {1, 0, 0} (dot) Vector {0, 1, 0} == 0");
-	assertTrue(vectorSum3.dot(vectorSum3) == dotProduct3, "Vector {1, 1, 1} (dot) Vector {1, 1, 1} == 3");
-	assertError([&](){ vectorSum3.dot(smallVector); }, "Vector {1, 1, 1} (dot) Vector {}");
+	assertTrue(vectorSum3.dot(identityVector1) == dotProduct1, "{1, 1, 1} (dot) {1, 0, 0} == 1");
+	assertTrue(identityVector1.dot(identityVector2) == dotProduct2, "{1, 0, 0} (dot) {0, 1, 0} == 0");
+	assertTrue(vectorSum3.dot(vectorSum3) == dotProduct3, "{1, 1, 1} (dot) {1, 1, 1} == 3");
+	assertError([&](){ vectorSum3.dot(smallVector); }, "{1, 1, 1} (dot) Vector(0)");
 
 	return 0;
 }
