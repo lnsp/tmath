4d7fe291ea97caafd502906bcae43446672f549e
Lennart Espe <lennart@mooxmirror.io>
2016-09-29 11:00:24 +0200

    test: add floor asserts

--- test/fcm.cpp
+++ test/fcm.cpp
@@ -1,5 +1,18 @@
 /* FLOOR, CEIL AND MODULO NEED TESTS. */
 
+#include "tmath.hpp"
+#include "tmath_test.hpp"
+
 int main(void) {
-    return 0;
+	using TMathTest::assert;
+
+	// testing floor
+	using TMath::floor;
+	assert(floor(0.0), 0, "floor(0.0) == 0");
+	assert(floor(0.25), 0, "floor(0.25) == 0");
+	assert(floor(1.0), 1, "floor(1.0) == 1");
+	assert(floor(-1.0), -1, "floor(-1.0) == -1");
+	assert(floor(-1.25), -2, "floor(-1.25) == -2");
+
+	return 0;
 }
