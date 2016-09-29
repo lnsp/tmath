4b4547bc69bba549d2dcfe6e155b7fba37a691fc
Lennart Espe <lennart@mooxmirror.io>
2016-09-29 11:31:34 +0200

    test: add mod asserts

--- test/fcm.cpp
+++ test/fcm.cpp
@@ -22,5 +22,13 @@ int main(void) {
 	assert(ceil(-1.0), -1, "ceil(-1.0) == -1");
 	assert(ceil(-1.25), -1, "ceil(-1.25) == -1");
 
+	// testing mod
+	using TMath::mod;
+	assert(mod(3.0, 4.0), 3.0, "mod(3.0, 4.0) == 3.0");
+	assert(mod(4.0, 4.0), 0.0, "mod(4.0, 4.0) == 0.0");
+	assert(mod(-2.0, 3.0), 1.0, "mod(-2.0, 3.0) == 1.0");
+	assert(mod(0.25, 0.75), 0.25, "mod(0.25, 0.75) == 0.25");
+	assert(mod(-0.9, -0.6), -0.3, "mod(-0.9, -0.6) == -0.3");
+
 	return 0;
 }
