44d7ccc9903d9abdfcb5b254f90009a85d434b02
Lennart Espe <lennart@mooxmirror.io>
2016-10-03 21:28:40 +0200

    feat: add empty constructor

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -2,12 +2,21 @@
 #include <iostream>
 
 TMath::Matrix::Matrix(std::initializer_list<std::initializer_list<DOUBLE>> list) {
-	size_t width = list.size(), index = 0;
-	if (width < 1) throw TMath::DIMENSION_ERROR;
+	size_t height = list.size(), index = 0;
+	if (height < 1) throw TMath::DIMENSION_ERROR;
 
-	elements = std::vector<std::vector<DOUBLE>>(width);
+	elements = std::vector<std::vector<DOUBLE>>(height);
 	for (auto& row : list) elements[index++] = std::vector<DOUBLE>(row.size());
 
 	size_t common = elements[index-1].size();
 	for (auto& row : elements) if (row.size() != common) throw TMath::DIMENSION_ERROR;
+}
+
+TMath::Matrix::Matrix(const int& width, const int& height) {
+	if (width < 1 || height < 1) throw TMath::DIMENSION_ERROR;
+
+	elements = std::vector<std::vector<DOUBLE>>(height);
+	for (size_t index = 0; index < width; index++) {
+		elements[index] = std::vector<DOUBLE>(width);
+	}
 }
