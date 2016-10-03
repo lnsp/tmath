0487ab5c18c0eb76556f4126d87a3fbebde4af5f
Lennart Espe <lennart@mooxmirror.io>
2016-10-03 23:18:48 +0200

    test: add asserts for added matrix member methods

--- test/matrix.cpp
+++ test/matrix.cpp
@@ -4,6 +4,7 @@ This test checks the matrix functionality.
 
 #include "tmath.hpp"
 #include "tmath_test.hpp"
+#include <iostream>
 
 int main(int argc, char const *argv[]) {
 	using TMathTest::assertTrue;
@@ -13,7 +14,35 @@ int main(int argc, char const *argv[]) {
 
 	Matrix nullMatrix1(1, 1);
 	Matrix nullMatrix2{{0}};
+	Matrix identityMatrix{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
 	
+	// Check for constructor errors
+	assertTrue(nullMatrix1 == nullMatrix2, "{{0}} == Matrix(1, 1)");
+	assertTrue(!(nullMatrix1 != nullMatrix2), "!{{0}} != Matrix(1, 1)");
+	assertError([&](){ nullMatrix1 == identityMatrix; }, "Dimension mismatch by 1x1 == 3x3");
+	assertError([&](){ nullMatrix1 != identityMatrix; }, "Dimension mismatch by 1x1 == 3x3");
 	assertError([&](){ Matrix(0, 0); }, "Empty matrix constructor using dimensions");
 	assertError([&](){ Matrix{}; }, "Empty matrix constructor using initializer list");
+
+	// Check for copy constructor
+	assertTrue(identityMatrix == Matrix(identityMatrix), "m == Matrix(m)");
+	assertTrue(nullMatrix1 == Matrix(nullMatrix2), "{{0}} == Matrix({{0}})");
+
+	// Check for access operator
+	assertTrue(nullMatrix1[0][0] == 0, "{{0}}[0][0] == 0");
+	assertTrue(identityMatrix[0][0] == 1, "Identity[0][0] == 1");
+	assertTrue(identityMatrix[1][0] == 0, "Identity[1][0] == 0");
+	assertTrue(identityMatrix[1][1] == 1, "Identity[1][1] == 1");
+
+	// Check for width and heiht
+	assertTrue(nullMatrix1.width() == 1, "{{0}}.width() == 1");
+	assertTrue(nullMatrix2.height() == 1, "Matrix(1, 1).height() == 1");
+	assertTrue(identityMatrix.width() == 3, "Identity.width() == 3");
+	assertTrue(identityMatrix.height() == 3, "Identity.height() == 3");
+
+	// Check for to_string
+	assertTrue(nullMatrix1.to_string() == "{[0]}", "str({{0}}) == '{[0]}'");
+	assertTrue(identityMatrix.to_string() == "{[1, 0, 0], [0, 1, 0], [0, 0, 1]}", "str(Identity) == '{[1, 0, 0], [0, 1, 0], [0, 0, 1]}'");
+
+	return 0;
 }
