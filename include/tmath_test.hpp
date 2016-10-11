d4b1e8d99eb1cba6a414cb54b55dc1922950137c
Lennart Espe <lennart@mooxmirror.io>
2016-10-11 21:33:23 +0200

    test: add assert for matrices

--- include/tmath_test.hpp
+++ include/tmath_test.hpp
@@ -10,6 +10,8 @@ const TMath::DOUBLE DEFAULT_TOLERANCE = 0.001;
 bool equal(TMath::DOUBLE x, TMath::DOUBLE y, TMath::DOUBLE tolerance);
 void assert(TMath::DOUBLE value, TMath::DOUBLE correct, std::string expression);
 void assert(TMath::DOUBLE value, TMath::DOUBLE correct, std::string expression, TMath::DOUBLE tolerance);
+void assert(TMath::Matrix a, TMath::Matrix b, std::string expression);
+void assert(TMath::Matrix a, TMath::Matrix b, std::string expression, TMath::DOUBLE tolerance);
 void assertTrue(bool b, std::string expression);
 void assertError(std::function<void ()>, std::string expression);
 }
