6657acc98433e4c1843b7a8996852b06449a627a
Lennart Espe <lennart@mooxmirror.io>
2016-10-15 10:09:45 +0200

    Add documentation for matrix class in class code and README (#48)
    * docs: add inline docs to matrix.cpp
    
    * docs: add matrix docs to README

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,6 +1,7 @@
 #include "tmath.hpp"
 #include <sstream>
 
+// Initialize a new 2d matrix with these values.
 TMath::Matrix::Matrix(std::initializer_list<std::initializer_list<DOUBLE>> list) {
 	size_t height = list.size(), index = 0;
 	if (height < 1) throw TMath::EMPTY_MATRIX_ERROR;
@@ -12,6 +13,7 @@ TMath::Matrix::Matrix(std::initializer_list<std::initializer_list<DOUBLE>> list)
 	for (auto& row : elements) if (row.size() != common) throw TMath::DIMENSION_ERROR;
 }
 
+// Initialize a new matrix with a specific width and height.
 TMath::Matrix::Matrix(const int& width, const int& height) {
 	if (width < 1 || height < 1) throw TMath::EMPTY_MATRIX_ERROR;
 
@@ -21,22 +23,26 @@ TMath::Matrix::Matrix(const int& width, const int& height) {
 	}
 }
 
+// Initialize a new matrix as a copy of the matrix m.
 TMath::Matrix::Matrix(const Matrix& m) {
 	size_t height = m.elements.size();
 	elements = std::vector<std::vector<DOUBLE>>(m.elements.size());
 	for (size_t index = 0; index < height; index++) elements[index] = std::vector<DOUBLE>(m.elements[index]);
 }
 
+// Check if the two matrices have the same dimensions.
 std::pair<size_t, size_t> TMath::Matrix::validate(const Matrix& m) const {
 	size_t w = width(), h = height();
 	if (m.width() != w || m.height() != h) throw TMath::DIMENSION_ERROR;
 	return std::pair<size_t, size_t>(w, h);
 }
 
+// Access a matrix row.
 std::vector<TMath::DOUBLE>& TMath::Matrix::operator[](const int& i) {
 	return elements[i];
 }
 
+// Test if the matrices are equal or nearly equal as specificed by the epsilon.
 bool TMath::Matrix::equal(const Matrix& m, const DOUBLE& eps) const {
 	std::pair<size_t, size_t> dimensions = validate(m);
 
@@ -46,22 +52,27 @@ bool TMath::Matrix::equal(const Matrix& m, const DOUBLE& eps) const {
 	return true;
 }
 
+// Test if the matrices are equal.
 bool TMath::Matrix::operator==(const Matrix& m) const {
 	return equal(m, EQUAL_EPSILON);
 }
 
+// Test if the matrices are unequal.
 bool TMath::Matrix::operator!=(const Matrix& m) const {
 	return !equal(m, EQUAL_EPSILON);
 }
 
+// Get the matrix col count.
 size_t TMath::Matrix::width() const {
 	return elements[0].size();
 }
 
+// Get the matrix row count.
 size_t TMath::Matrix::height() const {
 	return elements.size();
 }
 
+// Add two matrices.
 TMath::Matrix TMath::Matrix::operator+(const Matrix& a) const {
 	size_t w = width(), h = height();
 	if (a.width() != w || a.height() != h) throw TMath::DIMENSION_ERROR;
@@ -76,6 +87,7 @@ TMath::Matrix TMath::Matrix::operator+(const Matrix& a) const {
 	return result;
 }
 
+// Subtract two matrices.
 TMath::Matrix TMath::Matrix::operator-(const Matrix& a) const {
 	size_t w = width(), h = height();
 	if (a.width() != w || a.height() != h) throw TMath::DIMENSION_ERROR;
@@ -90,6 +102,7 @@ TMath::Matrix TMath::Matrix::operator-(const Matrix& a) const {
 	return result;
 }
 
+// Generate a string representation of the matrix.
 std::string TMath::Matrix::to_string() const {
 	size_t w = width(), h = height();
 	std::stringstream stream;
